5c54854c31455849bf85d92522adeffe3abd18d0
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-14 02:33:40 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -17,8 +17,8 @@ function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
   console.log('=        NODEPAY NETWORK BOT           ='.cyan);
-  console.log('=        Welcome & Enjoy Sir!          ='.cyan);
-  console.log('=        Author : Nofan Rambe          ='.cyan);
+  console.log('=        WELCOME & ENJOY SIR           ='.cyan);
+  console.log('=        AUTHOR : NOFAN RAMBE          ='.cyan);
   console.log('========================================'.cyan);
   console.log();
 }
