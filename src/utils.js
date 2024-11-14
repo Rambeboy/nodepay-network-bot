8ed41d2f18db870d3841900cfd83e55a3bcced4e
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-14 11:13:49 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -16,8 +16,8 @@ async function readLines(filename) {
 function displayHeader() {
   process.stdout.write('\x1Bc');
   console.log('========================================'.cyan);
-  console.log('=        NODEPAY NETWORK BOT           ='.cyan);
-  console.log('=        WELCOME & ENJOY SIR           ='.cyan);
+  console.log('=         NODEPAY NETWORK BOT          ='.cyan);
+  console.log('=         WELCOME & ENJOY SIR          ='.cyan);
   console.log('=        AUTHOR : NOFAN RAMBE          ='.cyan);
   console.log('========================================'.cyan);
   console.log();
