96229634b46ed3201de2f40284fcdda88b719453
R4MB3B0Y <kingindoslayer@gmail.com>
2024-12-01 00:39:04 +0700

    Update config.js

--- app/src/config/config.js
+++ app/src/config/config.js
@@ -2,11 +2,7 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = [
-    "http://18.142.29.174/api/network/ping",
-    "http://13.215.134.222/api/network/ping",
-    "http://54.255.192.166/api/network/ping",
-  ],
+    this.pingURL = 'http://104.22.39.228/api/network/ping',
     this.retryInterval = 30000;
     this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
   }
